511dd121e56b0e6a178f2e0eb5e9219e51ae1176
Evgeniy Guk <jekiee007@yandex.by>
2022-03-31 12:30:32 +0300

    fixed bug in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -56,7 +56,7 @@ function App() {
 
   const handleCardLike = (card) => {
     // Снова проверяем, есть ли уже лайк на этой карточке
-    const isLiked = card.likes.some((i) => i._id === currentUser._id);
+    const isLiked = card.likes.some((i) => i._id !== currentUser._id);
     // Отправляем запрос в API и получаем обновлённые данные карточки
     api
       .changeLikeCardStatus(card._id, !isLiked)
